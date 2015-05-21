ec460ea5a66749a0cebf49f257f952f82fc2da1d
Aviah <aviahl@gmail.com>
2015-05-21 04:47:41 +0300

    db, user sql same script

--- scripts/django_user.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER 'django'@'localhost' IDENTIFIED BY 'djangomysqlpassword';
-GRANT SELECT,INSERT,UPDATE,DELETE,CREATE TEMPORARY TABLES,LOCK TABLES, EXECUTE ON 'django_db.*' TO 'django'@'localhost';
-FLUSH PRIVILEGES;
