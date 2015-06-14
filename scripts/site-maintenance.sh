0a3a7f2491c6d7dbb3edb2cc09d904d0269cb9b3
Aviah <aviahl@gmail.com>
2015-06-14 12:47:02 +0300

    nginx

--- scripts/site-maintenance.sh
+++ scripts/site-maintenance.sh
@@ -3,4 +3,4 @@ echo "Site will go maintenance in 1 minute"
 sleep 60
 rm /etc/nginx/sites-enabled/*
 ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
-service nginx reload
+service nginx restart
