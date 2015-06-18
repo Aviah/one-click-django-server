26f58d42dc06e096b73b68012ac73da267dee876
Aviah <aviahl@gmail.com>
2015-06-18 11:23:03 +0300

    nginx proxy

--- site_repo/utils/requests.py
+++ site_repo/utils/requests.py
@@ -4,8 +4,7 @@
 def get_ip(request):
     
     ip = request.META['REMOTE_ADDR']
-    if not ip or not ip == '127.0.0.1':
-        ip = request.META.get('HTTP_X_FORWARDED_FOR',ip)
+    ip = request.META.get('HTTP_X_FORWARDED_FOR',ip)
         
     return ip
         
